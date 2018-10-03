e720621e1b84a7f23b23c7713323f0108cc81974
Alex Moonk <conte_alex@mail.ru>
2018-10-03 17:22:18 +0300

    Add src

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        boolean isActive = false;
+
+        byte a = 3;
+        short b = 75;
+        int c = 987;
+        long d = 987987;
+
+        double x = 6.7;
+        float y = 5.6F;
+
+        char m = 105;
+
+        String hi = "Hello, how low? ";
+
+
+        int e = 15;
+        int r = 20;
+        int u = a - b;
+        int i = a / b;
+
+        double p = 34.5 / 2.5;
+
+        int l = a + 8 * b++;
+
+        int k = b % a;
+
+
+
+
+
     }
 }
